bee79b8ab10dbbeca81ed1603168a3573f7b4422
AmyHuang82 <a3315520@gmail.com>
2024-07-16 15:24:19 +0800

    chore: add stylelint fix to lint staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,18 @@
 const path = require('path')
 
+function parseFiles(filenames) {
+  return filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')
+}
+
 function buildEslintCommand(filenames) {
-  return `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+  return `next lint --fix --file ${parseFiles(filenames)}`
+}
+
+function buildStylelintCommand(filenames) {
+  return `stylelint --fix --quiet ${parseFiles(filenames)}`
 }
 
 module.exports = {
   '*.{js,jsx}': [buildEslintCommand],
+  '*.{css,scss}': [buildStylelintCommand],
 }
