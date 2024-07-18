c39e127ae9816c7c588f1bdf774ac52f8a9353b4
AmyHuang82 <a3315520@gmail.com>
2024-07-18 16:07:11 +0800

    feat: add canvas parent container to control canvas size

--- components/3DObject/Scene.js
+++ components/3DObject/Scene.js
@@ -2,18 +2,21 @@ import { Suspense } from 'react'
 import { Environment, OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import Object from './Object'
+import styles from './Scene.module.scss'
 
 function Scene() {
   return (
-    <Canvas>
-      <OrbitControls position={[50, 10, 50]} />
-      <Suspense fallback={null}>
-        <Environment preset="city" />
-      </Suspense>
-      <Suspense fallback={null}>
-        <Object />
-      </Suspense>
-    </Canvas>
+    <div className={styles.canvasContainer}>
+      <Canvas>
+        <OrbitControls position={[50, 10, 50]} />
+        <Suspense fallback={null}>
+          <Environment preset="city" />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Object />
+        </Suspense>
+      </Canvas>
+    </div>
   )
 }
 
