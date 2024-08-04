159a97e4470ee0c7b1a39db32fe2e3afc24e41ac
AmyHuang82 <a3315520@gmail.com>
2024-08-04 13:09:10 +0800

    feat: implement basic `Panorama`

--- components/Panorama/Panorama.js
+++ components/Panorama/Panorama.js
@@ -1,8 +1,30 @@
+import dynamic from 'next/dynamic'
+import 'pannellum-react/es/pannellum/css/pannellum.css'
+
+const Pannellum = dynamic(() => import('pannellum-react').then((mod) => mod.Pannellum), {
+  ssr: false,
+})
+
 function Panorama() {
   return (
-    <div>
-      <h1>Panorama</h1>
-    </div>
+    <Pannellum
+      width="100%"
+      height="100vh"
+      image="/panorama/demo.jpg"
+      pitch={10}
+      yaw={180}
+      hfov={110}
+      autoLoad
+      showZoomCtrl={false}
+    >
+      <Pannellum.Hotspot
+        type="custom"
+        pitch={12.41}
+        yaw={117.76}
+        handleClick={(evt, name) => console.log(name)}
+        name="image info"
+      />
+    </Pannellum>
   )
 }
 
