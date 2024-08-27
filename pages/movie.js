4538525c1f5058a5676c36f393d8864d21884113
Jessie.Ho <jessie.ho@agneovo.com>
2024-08-27 23:58:42 +0800

    feat: add photo and movie page

---
+++ pages/movie.js
@@ -0,0 +1,10 @@
+import Box from '@/components/Box/Box'
+import Carousel from '@/components/Movie/Carousel'
+
+export default function MovieCarouselPage() {
+  return (
+    <Box>
+      <Carousel />
+    </Box>
+  )
+}
