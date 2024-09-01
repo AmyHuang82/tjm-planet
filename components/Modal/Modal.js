3bff827f7ec9301d554a58897b09cb24d5dadadc
AmyHuang82 <a3315520@gmail.com>
2024-09-01 18:19:52 +0800

    feat: change `Modal` to portal

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
+import { createPortal } from 'react-dom'
 import Cross from './Cross'
 import styles from './Modal.module.scss'
 
@@ -11,15 +12,27 @@ function Modal({ children, onCancel, ...props }) {
     }
   }
 
-  return (
+  const portalContainerRef = useRef(document.createElement('div'))
+  useEffect(() => {
+    const portalContainer = portalContainerRef.current
+    document.body.appendChild(portalContainer)
+
+    return () => {
+      document.body.removeChild(portalContainer)
+    }
+  }, [])
+
+  return createPortal(
     <div ref={maskRef} className={styles.mask} onClick={onMaskClick}>
       <button className={styles.closeButton} onClick={onCancel}>
         <Cross />
       </button>
+
       <div className={styles.modal} {...props}>
         {children}
       </div>
-    </div>
+    </div>,
+    portalContainerRef.current
   )
 }
 
