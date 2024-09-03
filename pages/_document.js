2eef55764a7e5cc3103abcdac06d4d711e083ab4
AmyHuang82 <a3315520@gmail.com>
2024-09-03 22:42:07 +0800

    feat: update fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=LXGW+WenKai+Mono+TC:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Script src="https://aframe.io/releases/1.6.0/aframe.min.js" strategy="beforeInteractive" />
         <Main />
